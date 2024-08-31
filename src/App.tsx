fbb0c9cc8d98da183783a50049a52aee43351d1e
Owais Khan <owaisnoorj@gmail.com>
2024-08-31 21:50:40 +0500

    make web responsive

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Todos from "./compoonents/todos2"
 function App() {
   return (
     <main>
-      <h1>Hi welcome to my first react todo web</h1>
       <h1>TODO REACT + TYPESCRIPT </h1>
       <Navbar/>
       <AddToDo/>
--- src/compoonents/navbar.tsx
+++ src/compoonents/navbar.tsx
@@ -5,12 +5,11 @@ function Navbar() {
     let todos̥Data = searchParams.get("todos");
   return (
     <nav>
-           <nav>
     <Link to="/" className={todos̥Data === null ? "active" : ""} > All </Link>
     <Link to="/?todos=active" className={todos̥Data === "active" ? "active" : ""} > Active </Link>
     <Link to="/?todos=completed" className={todos̥Data === "completed" ? "active" :""} > Completed </Link>
    </nav>
-    </nav>
+
 
   )
 }
